4618c2cacd82d73cd522e81c2210a689d2a3de22
ChefyHase <ChefyHase@example.com>
2019-05-22 22:38:35 +0900

    minor update

--- bin/gensouRipper.js
+++ bin/gensouRipper.js
@@ -9,7 +9,7 @@ const path = require('path');
 let novelUrl, output = undefined;
 
 program
-  .version('0.1.0')
+  .version('0.1.2')
   .arguments('<url> [path]')
   .action((url, path) => {
     novelUrl = url;
